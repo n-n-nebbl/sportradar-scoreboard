bbe7ebe42d0a925c06434e184a58317a75db8880
Alexander Nebel <alex.nebel@gmail.com>
2023-10-05 11:13:40 +0200

    adding test cases to verify ordering of matches in the summary

--- scoreboard/src/test/java/at/nebel/scoreboard/ScoreboardSummaryTest.java
+++ scoreboard/src/test/java/at/nebel/scoreboard/ScoreboardSummaryTest.java
@@ -2,23 +2,12 @@ package at.nebel.scoreboard;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mockito;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class ScoreboardSummaryTest extends AbstractScoreboardTest {
-  @Spy private ScoreboardRepository repository = new ScoreboardRepositoryInMemoryImpl();
-  @InjectMocks private ScoreboardServiceImpl scoreboard;
-
-  @BeforeEach
-  void resetMocks() {
-    Mockito.reset(repository);
-  }
 
   @Test
   void verifySummaryStartsEmpty() {
@@ -32,5 +21,31 @@ class ScoreboardSummaryTest extends AbstractScoreboardTest {
     var summary = scoreboard.listLiveMatches();
     assertThat(summary).isNotEmpty().hasSize(1);
     assertThat(summary.get(0).getKey()).isEqualTo(matchKey);
+  }
+
+  @Test
+  void verifyOrderingOfMatchesWithSameScore() {
+    var matchKeyEarlier = scoreboard.startMatch(HOME_TEAM_KEY, AWAY_TEAM_KEY);
+    var matchKeyLater = scoreboard.startMatch("123", "321");
+
+    var summary = scoreboard.listLiveMatches();
+
+    assertThat(summary).isNotEmpty().hasSize(2);
+    assertThat(summary.get(0).getKey()).isEqualTo(matchKeyLater);
+    assertThat(summary.get(1).getKey()).isEqualTo(matchKeyEarlier);
+  }
+
+  @Test
+  void verifyOrderingOfMatchesWithDifferentScore() {
+    var matchKeyEarlier = scoreboard.startMatch(HOME_TEAM_KEY, AWAY_TEAM_KEY);
+    var matchKeyLater = scoreboard.startMatch("123", "321");
+
+    //    scoreboard.updateScore(matchKeyEarlier, new MatchScore(1, 0));
+
+    var summary = scoreboard.listLiveMatches();
+
+    assertThat(summary).isNotEmpty().hasSize(2);
+    assertThat(summary.get(0).getKey()).isEqualTo(matchKeyEarlier);
+    assertThat(summary.get(1).getKey()).isEqualTo(matchKeyLater);
   }
 }
